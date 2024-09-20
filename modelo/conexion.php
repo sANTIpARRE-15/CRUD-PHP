01480948ad21a174a03483d22d65ef80c3c3c701
Santi <pizzu.fafa@gmail.com>
2024-09-20 13:10:04 -0300

    se documento el codigo de conexion con la base de datos

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,4 +1,8 @@
 <?php 
-$conexion=new mysqli("localhost", "root", "", "crud",);
-$conexion->set_charset ("utf8");
-?>
+// Creamos una nueva conexión a la base de datos usando MySQLi
+// "localhost" es el servidor local, "root" es el usuario, "" es la contraseña (vacía por defecto),
+// y "crud" es el nombre de la base de datos a la que nos conectamos.
+$conexion = new mysqli("localhost", "root", "", "crud");
+
+// Establecemos el formato de caracteres UTF-8 para asegurarnos de que los caracteres especiales (como acentos) se manejen correctamente.
+$conexion->set_charset("utf8");
